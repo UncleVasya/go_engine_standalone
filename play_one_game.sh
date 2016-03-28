fdafd13430a0f26cea071a9a21e0db18f59f7469
Jude Hungerford <edward.h@gmx.net>
2016-03-28 15:04:43 +1100

    add readme, remove some cruft

--- play_one_game.sh
+++ play_one_game.sh
@@ -3,6 +3,4 @@
 ./playgame.py --verbose --fill --log_input --log_output --log_error \
 --log_dir game_logs  --log_stderr --turns 600 --turntime 500 \
 "python starter/python/main.py" "python starter/python/main.py"
-#./starter/ocaml/main.native ./starter/ocaml/main.native
-#--map_file maps/tron_00.map \
 
