1ce5d4eca589ccd1ae1e806bd3e5d4c840ae49ce
Jude Hungerford <edward.h@gmx.net>
2016-04-06 14:28:02 +1000

    change play_one_game settings to 500 turns with 200ms per turn

--- play_one_game.sh
+++ play_one_game.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./playgame.py --verbose --fill --log_input --log_output --log_error \
---log_dir game_logs  --log_stderr --turns 600 --turntime 500 \
+--log_dir game_logs  --log_stderr --turns 500 --turntime 200 \
 "python starter/python/main.py" "python starter/python/main.py"
 
