2fd4f7daff81522a5cb4e2f1ce7795e657b13d27
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-04-11 10:36:02 +0300

    Windows support

---
+++ play_one_game.cmd
@@ -0,0 +1,5 @@
+python playgame.py --verbose --fill --log_input --log_output --log_error^
+ --log_dir game_logs --log_stderr --turns 500 --turntime 200^
+ "python starter/python/main.py" "python starter/python/main.py"
+ 
+pause
